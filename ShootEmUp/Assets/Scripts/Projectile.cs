bb6c128e7f118d0769cda3d9fbd3057c10770a31
Beber <alexandre.bernard94@gmail.com>
2019-10-19 13:41:52 +0200

    gun and bullet

---
+++ ShootEmUp/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float speed = 10;
+
+    public void setSpeed(float _newSpeed) {
+        speed = _newSpeed;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
