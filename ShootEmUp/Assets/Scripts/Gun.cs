a6a8f73625a1ac2962f5b72db095889925608919
Beber <alexandre.bernard94@gmail.com>
2020-03-14 12:01:49 +0100

    muzzleFlash + shells + enemyDeath effetcs

--- ShootEmUp/Assets/Scripts/Gun.cs
+++ ShootEmUp/Assets/Scripts/Gun.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public enum FireMode {AUTO, BURST, MULTIPLE};
+    public FireMode fireMode;
+
     public Transform muzzle;
     public Projectile projectile;
     public float msBetweenShots = 100;
     public float muzzleVelocity = 35;
-    
+
+    public Transform shell;
+    public Transform chamber;
+    MuzzleFlash muzzleFlash;
+
     float nextShotTime;
+
+    void Start() {
+        muzzleFlash = GetComponent<MuzzleFlash> ();
+    }
 
     public void Shoot() {
         if(Time.time > nextShotTime) {
             nextShotTime = Time.time + msBetweenShots / 1000;
             Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
             newProjectile.setSpeed(muzzleVelocity);     
+
+            Instantiate(shell, chamber.position, chamber.rotation);
+            muzzleFlash.Activate();
         }
     }
 
